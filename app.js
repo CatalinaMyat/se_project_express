a0d9ab639393033747ebcdce06565eeb0dec6338
PracticumTutor <PracticumTutor@gmail.com>
2025-10-25 19:40:38 -0400

    added /crash-test route for code review

--- app.js
+++ app.js
@@ -56,6 +56,12 @@ app.use(express.json());
 // request logger FIRST (added line)
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use("/", routes);
 
 // error logger AFTER routes, BEFORE error handlers (added line)
